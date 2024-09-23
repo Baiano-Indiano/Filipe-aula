28cb72d247601e2ad47f102d0b2e77b2ad6cd5ab
b. <99291603+Baiano-Indiano@users.noreply.github.com>
2024-09-23 09:41:17 -0300

    Atividade dia 23/09/2024

---
+++ filipe/automovel/Main.java
@@ -0,0 +1,13 @@
+package automovel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+        Carro carro = new Carro();
+        carro.setMarca("BMW");
+        carro.setModelo("X6M competition");
+        carro.setAno(2024);
+        carro.exibirDetalhes();
+        
+    }
+}
---
+++ filipe/bicho/Animal.java
@@ -0,0 +1,29 @@
+package bicho;
+
+public class Animal {
+    private int peso;
+    private int idade;
+    private int membros;
+
+    public int getPeso() {
+        return peso;
+    }
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public int getMembros() {
+        return membros;
+    }
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+
+}
+    
+
